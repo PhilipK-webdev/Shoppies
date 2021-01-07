a76f4bc23784840e0cc0d6b4cd638ccdcebcd65b
PhilipK-webdev <philipk179@gmail.com>
2021-01-06 21:14:01 -0800

    create the input field from user

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
+import { useState, useEffect } from "react";
 import './App.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import InputField from './components/input/InputField';
+import Axios from "axios";
 
 function App() {
+
+  const [movieName, setMovieName] = useState("");
+  const [arrMovie, setArrMovie] = useState([]);
+  const apiKey = "270a451d";
+
+  const addMovie = (e) => {
+    let tempName = e.target.value;
+    setMovieName(tempName);
+
+  }
+  const submit = (e) => {
+    e.preventDefault();
+    let movies = [];
+    movies = Axios.get(`http://www.omdbapi.com/?s=${movieName}&type=movie&apikey=${apiKey}`);
+    movies.then((res) => {
+      let arr = [...res.data.Search];
+      console.log(arr);
+      setArrMovie([...arr]);
+    })
+      .catch((err) => console.log(err));
+
+  }
+
   return (
     <div className="container-fluid" id="container-main">
       <div className="row">
         <div className="col-3"></div>
         <div className="col-6 text-center">
           <Header />
-          <InputField />
+          <InputField submit={submit} addMovie={addMovie} movieName={movieName} />
           <Footer />
         </div>
         <div className="col-3"></div>
--- src/components/input/InputField.js
+++ src/components/input/InputField.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import "../input/InputField.css"
-const InputField = () => {
+const InputField = (props) => {
     return (
         <div>
             <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Search Movie" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                <button className="btn" type="button" id="button-addon2">Button</button>
+                <input type="text" className="form-control" placeholder="Search Movie" aria-label="Recipient's username" value={props.movieName} aria-describedby="button-addon2" onChange={props.addMovie} />
+                <button className="btn" type="button" id="button-addon2" onClick={props.submit}>Button</button>
             </div>
         </div>
     )
